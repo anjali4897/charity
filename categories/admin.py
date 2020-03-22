01aa3505ec6eee675f7df0d17695204ed9443f48
anjali4897 <50988208+anjali4897@users.noreply.github.com>
2020-03-22 11:02:09 +0530

    Add files via upload

--- categories/admin.py
+++ categories/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category,Causes
+from .models import Category,Causes,charity_list
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Causes)
+admin.site.register(charity_list)
